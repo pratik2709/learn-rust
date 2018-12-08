8a427b79ebb8a484db12b47b7f5cfe1a5fb4cca0
pratiksaxena <pratik2709@gmail.com>
2018-12-08 20:28:36 +0530

    intermediate for ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,15 +1,19 @@
 fn main() {
-    let s = String::from("hello");
-    println!("{} cross",s);
-    take_ownership(s);
+    let s1 = gives_ownership();
+    let s2 = String::from("hello world");
+    let s3 = takes_and_gives_back(s2);
 
     let x=5;
     makes_copy(x);
-    println!("Hello, world!");
 }
 
-fn take_ownership(some_string: String){
-    println!("{}", some_string);
+fn gives_ownership() -> String {
+    let s = String::from("hello");
+    s
+}
+
+fn takes_and_gives_back(some_string: String) -> String{
+    some_string
 
 }
 

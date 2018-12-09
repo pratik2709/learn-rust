e87825a165dfb67d2dbc92c4783c1ffe9d8eaa32
pratiksaxena <pratik2709@gmail.com>
2018-12-09 22:06:53 +0530

    experimenting with match case

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -8,4 +8,12 @@ fn main() {
     let v4 = Ipaddress::Ipaddrv4(0,0,0,0);
     let v6 = Ipaddress::Ipaddrv6(String::from("::!"));
     println!("{:?}", v4);
+    println!("This address:: {}", which_address(v4));
+}
+
+fn which_address(ipaddress:Ipaddress) -> u32{
+    match ipaddress {
+        Ipaddress::Ipaddrv4(_,_,_,_) => 1,
+        Ipaddress::Ipaddrv6(_) => 2,
+    }
 }

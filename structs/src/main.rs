8be65339d1bb2ffa8206cbd9614b6656585bb939
pratiksaxena <pratik2709@gmail.com>
2018-12-09 15:26:27 +0530

    can hold method

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -9,12 +9,25 @@ impl Rectangle {
     fn calculate_area(&self) -> u32{
         self.width * self.height
     }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool{
+        let te1 = self.calculate_area();
+        let te2 = rect.calculate_area();
+        if te1 > te2 {
+            true
+        }
+        else{
+            false
+        }
+    }
 }
 
 fn main() {
-    let rect = Rectangle { width: 13, height: 30};
+    let rect1 = Rectangle { width: 100, height: 100};
+    let rect2 = Rectangle { width: 500, height: 100};
 
-    println!("Area is:: {:?}", rect.calculate_area())
+//    println!("Area is:: {:?}", rect.calculate_area());
+    println!("Can hold:: {:?}", rect1.can_hold(&rect2));
 }
 
 

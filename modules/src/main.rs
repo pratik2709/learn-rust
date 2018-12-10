6cd8e222a33610efb558d9b00f2423e620d70eff
pratiksaxena <pratik2709@gmail.com>
2018-12-10 23:59:05 +0530

    intermediate for super

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,11 +1,21 @@
 mod plant{
     pub mod vegetable{
+
         pub fn test_mod(){
             println!("Testing modules");
+            super::super::plants_rise();
         }
+
+
     }
 }
 
 fn main() {
     plant::vegetable::test_mod();
+    //absolute path
+    crate::plant::vegetable::test_mod();
+}
+
+fn plants_rise(){
+    println!("intake carbon");
 }

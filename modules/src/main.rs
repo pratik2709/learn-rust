676d6dbc9e259f72bd6f8a79933386c0ef3ab702
pratiksaxena <pratik2709@gmail.com>
2018-12-11 00:29:20 +0530

    testing out public private

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,5 +1,19 @@
 mod plant{
     pub mod vegetable{
+        #[derive(Debug)]
+        pub struct plant_types{
+            name: String,
+            size: i32,
+        }
+
+        impl plant_types{
+            pub fn set_plant_details() -> plant_types{
+                plant_types{
+                    name:String::from("Tree"),
+                    size:100
+                }
+            }
+        }
 
         pub fn test_mod(){
             println!("Testing modules");
@@ -14,6 +28,8 @@ fn main() {
     plant::vegetable::test_mod();
     //absolute path
     crate::plant::vegetable::test_mod();
+    let t = plant::vegetable::plant_types::set_plant_details();
+    println!("{:?}", t)
 }
 
 fn plants_rise(){

bc65b5c1140ba4de087e2bb4ac401d44904a5bc4
pratiksaxena <pratik2709@gmail.com>
2018-12-23 17:25:13 +0530

    testing generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -4,18 +4,21 @@ struct gen <T,U>{
     age: U
 }
 
-//impl gen <T,U>{
-//    fn test_knowlege(){
-//        gen{
-//
-//        }
-//    }
-//}
+impl<T, U> gen<T,U>{
+    fn test_knowlege(&self) -> &T{
+        &self.name
+    }
+
+    fn test_knowlege1(&self) -> &U{
+        &self.age
+    }
+}
 
 fn main() {
     let test = gen{
         name: "Pratik",
         age: 32
     };
-    println!("{:?}", test);
+    println!("{:?}", test.test_knowlege());
+    println!("{:?}", test.test_knowlege1());
 }

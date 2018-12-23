e32d7487f9af5b3f91b1ce09428765a3208915e4
pratiksaxena <pratik2709@gmail.com>
2018-12-23 22:14:48 +0530

    implementing default traits

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,5 @@
+mod lib;
+
 #[derive(Debug)]
 struct gen<T, U> {
     name: T,
@@ -12,28 +14,25 @@ impl<T, U> gen<T, U> {
     fn test_knowlege1(&self) -> &U {
         &self.age
     }
-
-
 }
 
 impl gen<&str, i32> {
     fn find_age_times_10(&self) -> i32 {
         self.age * 10
     }
-
 }
 
 #[derive(Debug)]
-struct point<T,U>{
+struct point<T, U> {
     name: T,
-    age: U
+    age: U,
 }
 
-impl <T,U> point<T,U>{
-    fn mixup<V,W>(self, other:point<V,W>) -> point<T,W>{
-        point{
-            name:self.name,
-            age:other.age
+impl<T, U> point<T, U> {
+    fn mixup<V, W>(self, other: point<V, W>) -> point<T, W> {
+        point {
+            name: self.name,
+            age: other.age,
         }
     }
 }
@@ -48,16 +47,46 @@ fn main() {
     println!("{:?}", test.test_knowlege1());
     println!("{:?}", test.find_age_times_10());
 
-    let p = point{
+    let p = point {
         name: "pt",
-        age: "pw"
+        age: "pw",
     };
 
-    let p1 = point{
+    let p1 = point {
         name: 1,
-        age: 2
+        age: 2,
     };
     let p3 = p.mixup(p1);
 //    println!("{:?}", p);
     println!("{:?}", p3);
+
+    let n = NewsFeed {
+        location: String::from("india"),
+        date: String::from("1/2"),
+        content: String::from("fffff"),
+        author: String::from("prat"),
+    };
+    println!("{}",n.summarize());
+}
+
+
+struct NewsFeed {
+    location: String,
+    date: String,
+    content: String,
+    author: String,
+}
+
+impl Summary for NewsFeed {
+    fn summarize_author(&self) -> String {
+        format!("Summarising author:: {}, {}", self.location, self.date)
+    }
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String{
+        format!("{}", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
 }

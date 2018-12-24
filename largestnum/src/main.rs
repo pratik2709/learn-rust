5da589fe1f0a02c6246b49701fbed6dadbdcfc1d
pratiksaxena <pratik2709@gmail.com>
2018-12-24 23:40:03 +0530

    playing with traits

--- largestnum/src/main.rs
+++ largestnum/src/main.rs
@@ -1,12 +1,25 @@
 fn main() {
     println!("Hello, world!");
-    let num_list = vec![1,2,3];
-    largest_number(&num_list);
+    let num_list = vec![1,100,2,3];
+    let charec_list = vec!['a','b','d'];
+    println!("{}", largest_number(&charec_list));
+    println!("{}", largest_number_without_clone(&charec_list));
 }
 
-fn largest_number<T>(list: &[T]) -> T{
+fn largest_number<T: PartialOrd + Copy>(list: &[T]) -> T{
     let mut largest = list[0];
     for &item in list.iter(){
+        if largest < item {
+            largest = item;
+        }
+    }
+    largest
+
+}
+
+fn largest_number_without_clone<T: PartialOrd>(list: &[T]) -> &T{
+    let mut largest = &list[0];
+    for item in list.iter(){
         if largest < item {
             largest = item;
         }

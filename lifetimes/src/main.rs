64759e4515c25274771bc02afc0288e2d95be056
pratiksaxena <pratik2709@gmail.com>
2018-12-25 15:42:42 +0530

    testing lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,22 +1,24 @@
 fn main() {
-    let r;
-    {
-        let x = 5;
-        r = &x;
-    }
-
-    println!("{}", r);
+//    let r;
+//    {
+//        let x = 5;
+//        r = &x;
+//    }
+//
+//    println!("{}", r);
+    test_lifetimes();
 }
 
 fn test_lifetimes(){
     let s = String::from("long longer");
     {
         let news = "longest";
+        let result = longest_string(&s, &news);
     }
-    let result = longest_string(&s, &news);
+
 }
 
-fn longest_string(s1: &str, s2: &str) -> &str{
+fn longest_string<'a>(s1: &'a str, s2: &'a str) -> &'a str{
     if s1 > s2{
         s1
     }

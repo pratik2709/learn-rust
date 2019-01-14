bd57272133465f23fb6aebf69a8c8cc12e826ca4
pratiksaxena <pratik2709@gmail.com>
2019-01-14 12:07:19 +0530

    fixes

--- hexaparser/src/main.rs
+++ hexaparser/src/main.rs
@@ -5,11 +5,14 @@ fn main(){
     //first will be the name of the binary
     let args: Vec<String> = env::args().collect();
 
+    let (query, filename) = parse_args(&args);
+
+    let contents = fs::read_to_string(filename).expect("something went wrong");
+    println!("file contents:\n {}", contents);
+}
+
+fn parse_args(args: &[String]) -> (&str, &str){
     let query: &str = &args[1];
     let filename: &str = &args[2];
-    println!("Searching for {:?}", query);
-    println!("In filename {:?}", filename);
-
-    let contents = fs::read_to_string(filename);
-    println!("file contents: {:?}", contents);
+    (query, filename)
 }

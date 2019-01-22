b50ae241e411cc653c3720171583b464096a9b20
pratiksaxena <pratik2709@gmail.com>
2019-01-22 21:52:44 +0530

    regular expression for punctuation

--- hexaparser/src/lib.rs
+++ hexaparser/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate regex;
 use std::error::Error;
 use std::fs;
 use std::ascii;
@@ -6,10 +7,14 @@ use std::path::Path;
 use std::fs::File;
 use std::io::Write;
 
+use regex::Regex;
+
+
+
+
 pub fn run(config: Config) ->  Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     println!("file contents:\n {}", contents);
-//    println!("{}", encode_hex("foobar".as_bytes()));
 
     let actualPath = Path::new("output.txt");
     let mut actual_file = match File::create(&actualPath) {
@@ -17,12 +22,23 @@ pub fn run(config: Config) ->  Result<(), Box<dyn Error>> {
         Ok(actual_file) => actual_file,
     };
 
+
     match actual_file.write_all(encode_hex(contents.as_bytes()).as_bytes()){
         Err(why) => panic!("cannot write {}", why.description()),
         Ok(actual_file) => println!("Written")
     }
+
+    let re = Regex::new("[\\p{Punct}\\p{IsPunctuation}]").unwrap();
+    assert!(re.is_match("A"));
+
     Ok(())
 }
+
+//fn words_by_line(stringBuffer:&str){
+//    stringBuffer.lines().map(|line|{
+//        line.split_whitespace().collect()
+//    }).collect()
+//}
 
 pub fn encode_hex(bytes: &[u8]) -> String {
     use std::fmt::Write;

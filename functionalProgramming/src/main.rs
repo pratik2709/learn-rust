b92374fe5805aa5dd432fed025c870827d5a24fc
pratiksaxena <pratik2709@gmail.com>
2019-01-26 10:55:44 +0530

    creating a closure

--- functionalProgramming/src/main.rs
+++ functionalProgramming/src/main.rs
@@ -5,24 +5,25 @@ fn main(){
   generate_workout(1,2);
 }
 
-fn simulated_expensive_function(intensity: u32) -> u32 {
-  println!("inside simulated function");
-  thread::sleep(Duration::from_secs(2));
-  intensity
-}
+
 
 fn generate_workout(intensity:u32, random_number:u32){
-  if(intensity < 25){
-    simulated_expensive_function(intensity);
-  
-    println!("calling again:: {}", simulated_expensive_function(intensity));
+  let expensive_func = |intensity|{
+    println!("inside simulated function");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+  };
+
+  if intensity < 25 {
+    expensive_func(intensity);
+    println!("calling again:: {}", expensive_func(intensity));
   }
   else{
     if random_number == 3{
       println!("take a break");
     }
     else{
-      simulated_expensive_function(intensity);
+      expensive_func(intensity);
     }
   }
 

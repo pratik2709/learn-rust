83dd8d8398818e76a884f8e0bf8b5b12fb0d664c
pratiksaxena <pratik2709@gmail.com>
2020-01-15 17:33:57 +0530

    intermediate for rust linked list

--- linkedLists/src/first.rs
+++ linkedLists/src/first.rs
@@ -1,10 +1,12 @@
+use std::mem;
+
 pub struct List{
     head: Link
 }
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
 
 enum Link {
@@ -17,6 +19,28 @@ impl List{
         List {
             head: Link::Empty
         }
+    }
+
+    pub fn push(&mut self, elem:i32){
+        let new_node = Box::new(Node {
+            elem,
+            next: Link::Empty
+        });
+        //todo : mem replace
+        self.head = Link::More(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match &self.head {
+            Link::Empty => {
+
+            }
+            Link::More(node) => {
+
+            }
+        }
+
+        unimplemented!();
     }
 
 }

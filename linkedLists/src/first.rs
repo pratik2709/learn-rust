9d8cb043512ec72577783ff7c145feafeec62b96
pratiksaxena <pratik2709@gmail.com>
2020-01-16 23:38:56 +0530

    tests

--- linkedLists/src/first.rs
+++ linkedLists/src/first.rs
@@ -4,11 +4,13 @@ pub struct List{
     head: Link
 }
 
+#[derive(PartialEq)]
 struct Node {
     elem: i32,
     next: Link,
 }
 
+#[derive(PartialEq)]
 enum Link {
     Empty,
     More(Box<Node>)
@@ -24,7 +26,7 @@ impl List{
     pub fn push(&mut self, elem:i32){
         let new_node = Box::new(Node {
             elem,
-            next: Link::Empty
+            next: mem::replace(&mut self.head, Link::Empty)
         });
         //todo : mem replace
         self.head = Link::More(new_node)
@@ -40,4 +42,42 @@ impl List{
         }
     }
 
+//    pub fn disp(&mut self) {
+//        let newPtr = self.head;
+//        while self.head != Link::Empty {
+//            println!("{}" , 1);
+////            self.head = self.head.next
+//        }
+//    }
+
+}
+
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        // TODO
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+//        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }

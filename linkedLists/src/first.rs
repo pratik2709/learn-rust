f4a4017a425f55c33b8dc3a116a37ad1b867861a
pratiksaxena <pratik2709@gmail.com>
2020-01-18 10:56:54 +0530

    intermediate

--- linkedLists/src/first.rs
+++ linkedLists/src/first.rs
@@ -1,45 +1,53 @@
 use std::mem;
 
-pub struct List{
-    head: Link
+pub struct List<T> {
+    head: Link<T>
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(PartialEq)]
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List{
-    pub fn new() -> Self{
+impl<T> List<T> {
+    pub fn new() -> Self {
         List {
             head: None
         }
     }
 
-    pub fn push(&mut self, elem:i32){
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
-            next: self.head.take()
+            next: self.head.take(),
         });
         self.head = Some(new_node)
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
-        match self.head.take() {
-            None => None,
-            Some(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
@@ -50,6 +58,7 @@ impl Drop for List {
 
 mod test {
     use super::List;
+
     #[test]
     fn basics() {
         // TODO
@@ -59,6 +68,13 @@ mod test {
         list.push(1);
         list.push(2);
         list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|&mut val|{
+            val = 42;
+        });
+        assert_eq!(list.peek(), Some(&42));
 
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(2));

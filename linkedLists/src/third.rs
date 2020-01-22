ae10f8a5ff2660c45ffa31fffe846a3647e9d5fd
pratiksaxena <pratik2709@gmail.com>
2020-01-22 23:46:26 +0530

    tests pass

--- linkedLists/src/third.rs
+++ linkedLists/src/third.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-include!("test.rs");
+//include!("test.rs");
 
 pub struct List<T>{
     head: Link<T>,
@@ -27,5 +27,45 @@ impl<T> List<T> {
         }
     }
 
+    pub fn tail(&self) -> List<T>{
+        List{
+            head: self.head.as_ref().and_then(|node|{
+                node.next.clone()
+            })
+        }
+    }
 
+    pub fn head(&self) -> Option<&T>{
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+
+}
+
+#[cfg(test)]
+mod test{
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.append(1).append(2).append(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+    }
 }

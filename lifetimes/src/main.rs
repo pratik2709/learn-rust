dbb13ab22d9cc361bbc085a3107efb4fb1ed342c
Pratik Saxena <pratik2709@gmail.com>
2022-05-01 09:22:07 +0530

    created mutable iterator

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,28 +1,44 @@
 use std::mem;
-include!("static.rs");
+// include!("static.rs");
 
-fn main(){
-    execute();
-    let a = 10;
-    let b = 20;
-    let res = add_with_lifetimes(&a, &b);
-    println!("{}", res);
+// fn main(){
+//     execute();
+//     let a = 10;
+//     let b = 20;
+//     let res = add_with_lifetimes(&a, &b);
+//     println!("{}", res);
+// }
+//
+// fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32{
+//     *i + *j
+// }
+fn main() {}
+
+struct MutableIterator<'iter, T> {
+    slice: &'iter mut [T],
 }
 
-fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32{
-    *i + *j
-}
-
-struct MutableIterator<'iter, T>{
-    slice:&'iter mut [T]
-}
-
-impl <'iter, T> Iterator for MutableIterator<'iter, T>{
+impl<'iter, T> Iterator for MutableIterator<'iter, T> {
     type Item = &'iter mut T;
-    fn next<'next>(&'next mut self) -> Option<Self::Item>{
+    fn next(&mut self) -> Option<Self::Item> {
         let mut slice1 = mem::replace(&mut self.slice, &mut []);
         let (first, last) = slice1.split_first_mut()?;
         self.slice = last;
         Some(first)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mutable_iterator() {
+        let mut collection = vec![1, 2, 3, 4];
+        let mut iter_collection = MutableIterator {
+            slice: &mut collection
+        };
+        println!("{:?}", iter_collection.next());
+        assert_eq!(iter_collection.next().unwrap(), &2);
     }
 }

3c1906831458739fa248fa01389f69b46545beb5
Pratik Saxena <pratik2709@gmail.com>
2022-05-01 09:00:38 +0530

    intermediate for lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,4 @@
+use std::mem;
 include!("static.rs");
 
 fn main(){
@@ -10,4 +11,18 @@ fn main(){
 
 fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32{
     *i + *j
+}
+
+struct MutableIterator<'iter, T>{
+    slice:&'iter mut [T]
+}
+
+impl <'iter, T> Iterator for MutableIterator<'iter, T>{
+    type Item = &'iter mut T;
+    fn next<'next>(&'next mut self) -> Option<Self::Item>{
+        let mut slice1 = mem::replace(&mut self.slice, &mut []);
+        let (first, last) = slice1.split_first_mut()?;
+        self.slice = last;
+        Some(first)
+    }
 }

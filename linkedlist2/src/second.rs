3b9a2d5928fc9affc4da9aa9f2c6345e05d06aa2
Pratik Saxena <pratik2709@gmail.com>
2022-05-02 23:36:42 +0530

    intermediate for linked list

--- linkedlist2/src/second.rs
+++ linkedlist2/src/second.rs
@@ -10,7 +10,7 @@ struct Node{
 }
 
 impl List{
-    fn new(&mut self)-> Self{
+    fn new()-> Self{
         List{
             head: None
         }
@@ -22,5 +22,33 @@ impl List{
             next: std::mem::replace(&mut self.head,None)
         });
         self.head = Some(node);
+    }
+
+    fn pop(&mut self) -> Option<i32>{
+        match std::mem::replace(&mut self.head, None){
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.value)
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test{
+    use super::List;
+
+    #[test]
+    fn basics(){
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }

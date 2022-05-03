f39cc2cf2fc67a8302a38a2e38fcb250edc8d159
Pratik Saxena <pratik2709@gmail.com>
2022-05-03 23:20:21 +0530

    intermediate for iter

--- linkedlist2/src/second.rs
+++ linkedlist2/src/second.rs
@@ -88,4 +88,15 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
+
+    #[test]
+    fn iter_tests(){
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+    }
 }
